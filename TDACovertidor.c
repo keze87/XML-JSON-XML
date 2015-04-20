011861563f844f1363fa486196bcda3116dfc728
Karina <karinamatles@gmail.com>
2015-04-20 18:09:04 -0300

    Modifiqué el TDA
    Dejo listo para escribir dentro de cada primitiva

--- TDACovertidor.c
+++ TDACovertidor.c
@@ -1,14 +1,12 @@
-typedef struct TDAConvertidor{
-  TDAXML xml;
-  TDAJSON json;
-}TDAConvertidor;
+#include "TDACovertidor.h"
 
-/* PRE: No tiene.
-POST: Convierte xml a json según las rutas especificadas.
-En caso de error devuelve el mensaje correspondiente. */
-int xml2json(TDAConvertidor * tc, char * rutaXml, char * rutaJson);
+int xml2json(TDAConvertidor * tc, char * rutaXml, char * rutaJson){
 
-/* PRE: No tiene.
-POST: Convierte json a xml según las rutas especificadas.
-En caso de error devuelve el mensaje correspondiente. */
-int json2xml(TDAConvertidor * tc, char * rutaJson, char * rutaXml);
+}
+
+
+int json2xml(TDAConvertidor * tc, char * rutaJson, char * rutaXml){
+
+}
+
+
