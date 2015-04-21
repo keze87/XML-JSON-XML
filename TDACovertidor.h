6c250b34880c009ee3e093fa2e90d44dcee1cf5f
gtulipani <gtulipani@hotmail.com>
2015-04-21 17:59:06 -0300

    Agregar validación #ifndef e inclusiones
    - Agregar validacion #ifndef y #endif para TDAConvertidor.
    - Incluir las librerías "TDAXML.h" y "TDAJSON.h" para poder utilizar los tipos correspondientes en el struct TDAConvertidor.

--- TDACovertidor.h
+++ TDACovertidor.h
@@ -1,3 +1,9 @@
+#ifndef __TDAConvertidor_H__
+#define __TDAConvertidor_H__
+
+#include "TDAXML.h"
+#include "TDAJSON.h"
+
 typedef struct TDAConvertidor{
   TDAXML xml;
   TDAJSON json;
@@ -12,5 +18,7 @@ int xml2json(TDAConvertidor * tc, char * rutaXml, char * rutaJson);
 POST: Convierte json a xml según las rutas especificadas.
 En caso de error devuelve el mensaje correspondiente. */
 int json2xml(TDAConvertidor * tc, char * rutaJson, char * rutaXml);
+
+#endif /* __TDAConvertidor_H__ */
 
 
