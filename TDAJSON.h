41cb994192f8debac7fc759dcebe457e1ef2b7ac
Karina <karinamatles@gmail.com>
2015-04-26 17:45:17 -0300

    Nada, corregí un comentario

--- TDAJSON.h
+++ TDAJSON.h
@@ -14,7 +14,7 @@ POST: Inicializa el TDAJSON con el archivo xml que persiste en rutaJson. */
 int jsonCargar(TDAJSON *TDAJson, char *rutaJson);
 
 /* PRE: TDAJSON correctamente inicializado en memoria
-POST: Guarda el TDAJSON en el archivo xml que persistirá en rutaJson. */
+POST: Guarda el TDAJSON en el archivo json que persistirá en rutaJson. */
 int jsonGuardar(TDAJSON *TDAJson, char *rutaJson);
 
 #endif /* __TDAJSON_H__ */
