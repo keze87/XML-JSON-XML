4bf0c9dd4bb79003c3cf368ccbed52c584295c45
gtulipani <gtulipani@hotmail.com>
2015-05-02 15:23:14 -0300

    Asignacion de memoria a char * y modificaciones varias
    Modificaciones del otro día + cambios:
    - Toda la modificación de la función inicializar cómo hice el otro día
    - Agregado de dos nuevas constantes MAX_LONG_RUTA y C_NULO
    - Creado de nueva función InicializarString que asigna memoria a un char * y lo inicializa, o devuelve un error
    - Reemplazo de los "strcpy" por un "memcpy" para poder manejar el heap. Aclaración: además, es mejor que el strcpy, porque se puede restrinjir la cantidad de caracteres que se copian (en este caso lo hice con la constante definida anteriormente) y así evitamos lo que dijo el profesor sobre el hackeo (igual existe la función 'strncpy' que podría hacer lo mismo, pero no sé si funciona tan bien cuando se habla de memoria dinámica)
    
    Comentario: me parece que no está bueno llamar directamente desde el main a la función del TDAConvertidor, porque de esa forma no vamos a poder manejar el error con detalle, osea se me ocurrió que la función xml2json debería devolver int de la siguiente forma: 0 si sale ok, -1 por algún error de memoria, o un numero mayor a 0 que indica la línea en la que se produjo el error. Si llamamos directamente desde el main.c a la función, no podemos hacer que esta devuelva un tipo tError, y deberíamos trabajar con if ... == 0 y cosas por el estilo. Por eso me parece que estaría bueno hacer una función aparte que trabaje con las primitivas del TDAConvertidor y pueda manejar los errores.
    
    Todavía no lo pude compilar porque no me bajé el github, pero si quieren pruebenlo y si hay algún error corríjanlo directamente de acá.

--- main.c
+++ main.c
@@ -1,79 +1,138 @@
 #include <stdio.h>
 #include <string.h>
-
 #include "TDAConvertidor.h"
 
-#define XML2JSON 1
-#define JSON2XML 2
+#define MAX_LONG_RUTA 100
+#define C_NULO '\0'
 
-/*Lectura de argumentos*/
-/* Orden = el tipo de funcion que tengamos que usar.
- * posjson,posxml = el valor de argc que da a la ruta de cada uno*/
-void inicializar(int argc, char **argv, int *orden, int *posjson, int *posxml,int narg);
+#define OUT_OF_MEM "No existe suficiente memoria.\n"
+#define RUTA_ORIGEN "Debe ingresar una ruta de origen a la que se pueda acceder para lectura.\n Debe ubicarse seguido de la operación a realizar.\n"
+#define RUTA_DESTINO "Debe ingresar una ruta de destino a la que se pueda acceder para escritura.\n Debe ubicarse seguido de la ruta de origen.\n"
+
+typedef enum {
+    VACIO, /* Aún no se detectó la operación */
+    XML2JSON, /* Convertir XML a JSON */
+    JSON2XML, /* Convertir JSON a XML */
+    ERROR /* Argumento inválido */
+} tOperacion;
+
+typedef enum {
+    SUCCESS, /* Éxito */
+    MEM, /* Error de memoria */
+    ORIG, /* Error en la ruta de origen */
+    DEST /* Error en la ruta de destino */
+} tError;
+
+/* Funcion que se encarga de identificar la operación a realizar
+ * @param  arg  argumento ingresado por el usuario
+ * @param  orden  identificador de la operación
+ * @pos devuelve -1 si el argumento es incorrecto
+ */
+void Inicializar(char *arg, tOperacion *Ope)
+{
+    if ((strcmp(arg,"-xml2json") == 0))
+        *Ope = XML2JSON;
+    else
+    {
+        if ((strcmp(arg,"-json2xml") == 0))
+            *Ope = JSON2XML;
+        else
+            *Ope = ERROR;
+    }
+}
+
+/* Funcion que se encarga de mostrar un mensaje por pantalla de acuerdo al error */
+void MostrarMensajeError(tError Code)
+{
+    switch (Code)
+    {
+        case(MEM):
+        {
+            printf(OUT_OF_MEM);
+            break;
+        }
+        case(ORIG):
+        {
+            printf(RUTA_ORIGEN);
+            break;
+        }
+        case(DEST):
+        {
+            printf(RUTA_DESTINO);
+            break;
+        }
+        case(SUCCESS): break;
+    }
+}
+
+/* Funcion que se encarga de inicializar una cadena de caracteres char *
+ * @param  s   cadena de caracteres a inicializar
+ * @pos    devuelve MEM - error de memoria - si no puede asignar memoria
+ *         devuelve SUCCESS e inicializa la cadena si puede asignar memoria
+ */
+tError InicializarString(char* s)
+{
+    if ((s = (char*)malloc(MAX_LONG_RUTA)) == NULL) /* No pude asignar memoria */
+        return MEM;
+    memset(s,C_NULO,1); /* Inicializo la cadena */
+    return SUCCESS;
+}
 
 int main(int argc, char *argv[])
 {
-
-	int orden = 0;
-	int posjson = 0;
-	int posxml = 0;
-	int error = 0;
-	int narg = 1; /* Inicia el for*/
-
-	do
-	{
-
-		TDAConvertidor *tc = creartc();
-
-		inicializar (argc,argv,&orden,&posjson,&posxml,narg);
-
-		switch (orden)
-		{
-			case XML2JSON: error = xml2json(tc,argv[posxml],argv[posjson]); break;
-			case JSON2XML: error = json2xml(tc,argv[posjson],argv[posxml]); break;
-			default: fprintf(stderr,"Opcion incorrecta.\n");
-		}
-
-		free(tc);
-
-		narg = narg + 3;
-
-	}while (narg < argc);
-
-	return error;
-
-}
-
-void inicializar(int argc, char **argv, int *orden, int *posjson, int *posxml,int narg)
-{
-
-	int auxargc;
-
-	for (auxargc = narg; auxargc != argc; auxargc++)
-	{
-
-		if ((strcmp(argv[auxargc],"-xml2json") == 0) && (*orden == 0))
-			*orden = XML2JSON;
-		else
-		{
-			if ((strcmp(argv[auxargc],"-json2xml") == 0) && (*orden == 0))
-				*orden = JSON2XML;
-			else
-			{
-
-				if ((*orden == XML2JSON) && (*posxml == 0))
-				{
-					*posxml = auxargc;
-					*posjson = auxargc + 1;
-				}
-
-				if ((*orden == JSON2XML) && (*posjson == 0))
-				{
-					*posjson = auxargc;
-					*posxml = auxargc + 1;
-				}
-
-			}
-		}
-	}
+    TDAConvertidor* TC = NULL;
+    tOperacion Ope = VACIO;
+    tError Code = SUCCESS;
+    char* RutaOrigen;
+    char* RutaDestino;
+    int i = 1;
+    if (CrearTC(TC)!=0)
+        Code = MEM;
+    Code = InicializarString(RutaOrigen);
+    Code = InicializarString(RutaDestino);
+    if (Code == SUCCESS) {
+        if (argc == 1) /* El usuario no ingresó argumentos, además del nombre del programa */
+            printf("Debe ingresar al menos un argumento seguido del nombre del programa.\n");
+        else
+        {
+            do
+            {
+                Ope = VACIO;
+                Inicializar(argv[i],&Ope);
+                if (Ope != ERROR)
+                {
+                    if (i!=(argc-1))
+                    { /* No es el último argumento --> puedo obtener la ruta de origen */
+                        memcpy(RutaOrigen,argv[++i],MAX_LONG_RUTA);
+                        if (i!=(argc-1))
+                        { /* No es el último argumento --> puedo obtener la ruta de destino */
+                            memcpy(RutaDestino,argv[++i],MAX_LONG_RUTA);
+                        }
+                        else /* Es el último argumento y no tengo la ruta de destino */
+                            Code = DEST;
+                    }
+                    else /* Es el último argumento y no tengo la ruta de origen */
+                        Code = ORIG;
+                    switch (Ope)
+                    {
+                        case (XML2JSON):
+                        {
+                            Code = xml2json(TC,RutaOrigen,RutaDestino);
+                            break;
+                        }
+                        case (JSON2XML):
+                        {
+                            Code = json2xml(TC,RutaOrigen,RutaDestino);
+                            break;
+                        }
+                        default: break;
+                    }
+                }
+            } while ((++i < argc) && (Code==SUCCESS));
+        }
+        DestruirTC(TC);
+    }
+    if (Code != SUCCESS)
+        MostrarMensajeError(Code);
+    return EXIT_SUCCESS;
 }
