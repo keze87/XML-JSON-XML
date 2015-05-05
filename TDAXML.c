e5551e8c6166478ee78d63484332b88163351c7d
gtulipani <gtulipani@hotmail.com>
2015-05-05 13:26:35 -0300

    int xmlGuardar
    Gente, acá hicel el xmlGuardar. Aclaro por las dudas, que NO VA A COMPILAR, porque falta definir bien el tipo de elemento de la lista, pero les comento lo que yo pensé con la implementación.
    - El tag principal no se incluye en la lista de elementos (ya que tenemos el char* tagPrincipal).
    - Cada elemento de la lista es un tElem, que posee 3 campos: Tipo (enumerado que puede ser DELIM o ELEM, de acuerdo a si es un delimitador o un elemento), Atributo (independientemente del tipo de elemento, posee el nombre del atributo, es decir lo que se encuentra entre < y >; y por último Value (en caso de ser DELIM, posee el valor ABIERTO o CERRADO, y si es un ELEM el valor del atributo (no podemos trabajar con typedef enum por esto ultimo, ya que si es un elem puede tener cualquier valor...).
    
    Ejemplo: <id>file</id>
    Tipo=ELEM
    Atributo="id"
    Value="file"
    
    Ejemplo:<menuitem>
    Tipo=DELIM
    Atributo="menuitem"
    Value="ABIERTO"
    
    Les explico un poco la funcionalidad. Utilizo las siguientes funciones auxiliares, que NO SON PRIMITIVAS, por eso no las declaro en el ".h":
    - EscribirAtributo_Apertura: funcion que recibe el campo Atributo y FILE. Se encarga de grabar el atributo con el formato <atributo>
    - EscribirAtributo_Cierre: funcion que recibe el campo Atributo y FILE. Se encarga de grabar el atributo con el formato </atributo>
    - EscribirTabs: funcion que recibe un int y escribe en el archivo una determinada cantidad de tabs, para darle formato al xml
    - EsDelimitador: funcion que recibe el elemento de la lista, y verificar si es un DELIM o un ELEM, no hace falta crear un "EsElem" porque es un "o exclusivo", osea si no es un DELIM es un ELEM
    
    En cuanto a la funcion, lo primero que hace es crear un elemento auxiliar, donde depositare el resultado de "L_Elem_Cte". Utilizo una variable llamada "code" que utilizo para validar el resultado de mover el corriente (el primer mover corriente, cuando se mueve al primero no se valida, porque como la lista no esta vacia, nunca puede dar error). Además utilizo una variable llamada "nivel" que utilizo para saber la cantidad de "tabs" que tengo que dibujar en el archivo.
    
    Aún no la pude probar, pero si tiene algún error debe ser mínimo. Cualquier duda que tengan preguntenme.
    
    En cuánto a lo que estuvimos hablando del elemento de la lista, me parece que lo más fácil termina siendo lo que expliqué al principio. Además, podemos utilizar la funcion tElem_Inicializar también para el cargar
    
    Saludosss

--- TDAXML.c
+++ TDAXML.c
@@ -3,6 +3,71 @@
 #include "TDAConvertidor.h"
 
 #define CANTMAX 255 /*tamaño maximo de linea*/
+#define TAM_ELEM 200
+#define DELIM_OPEN "ABIERTO"
+#define DELIM_CLOSE "CERRADO"
+
+typedef enum {
+	ELEM, /* Es un elemento */
+	DELIM, /* Es un delimitador */
+	VACIO /* Aún no se definió */
+} tTipo
+
+typedef struct {
+	tTipo Tipo;
+	char *Atributo;
+	char *Value;
+} tElem;
+
+/* Inicializa tElem 
+ * Si no existe suficiente memoria para inicializar la estructura devuelve -1
+ */
+int tElem_Inicializar(tElem* aux)
+{
+	if ((tElem->Atributo = (char*)malloc(CANTMAX+1)) == NULL)
+		return -1;
+	if ((tElem->Value = (char*)malloc(CANTMAX+1)) == NULL) {
+		free(tElem->Atributo);
+		return -1;
+	}
+	tElem->Tipo=VACIO;
+}
+
+/* Libera la memoria utilizada por tElem */
+void tElem_Destruir(tElem* aux)
+{
+	free(tElem->Atributo);
+	free(tElem->Value);
+}
+
+/* Funcion que devuelve 0 si el elemento es un delimitador */
+int EsDelimitador (tElem aux)
+{
+	return (aux.Tipo == DELIM)?0:-1;
+}
+
+/* Funcion que escribe una cantidad de tabs en el archivo */
+void EscribirTabs(int cant, FILE* arch)
+{
+	while (cant-- > 0)
+		fputc('\t', arch);
+}
+/* Funcion que escribe la apertura de un atributo en un archivo */
+void EscribirAtributo_Apertura(char* at, FILE* arch)
+{
+	fputc('<', arch);
+	fputs(at, arch);
+	fputc('>', arch);
+}
+
+/* Funcion que escribe el cierre de un atributo en un archivo */
+void EscribirAtributo_Cierre(char* at, FILE* arch)
+{
+	fputc('<', arch);
+	fputc('/', arch);
+	fputs(at, arch);
+	fputc('>', arch);
+}
 
 int xmlCargar(TDAXML *TDAXml, char *rutaXml)
 {
@@ -173,21 +238,54 @@ int xmlCargar(TDAXML *TDAXml, char *rutaXml)
 
 }
 
-int TDAXML_Guardar(TDAXML *TDAXml, char *rutaXml);
+int xmlGuardar(TDAXML *TDAXml, char *rutaXml)
 {
-
-	FILE *archivoxml;
-	archivoxml = fopen(rutaXml, "w");
-	if (archivoxml)
-	{
-		printf("SE ABRIO!\n");
-		fclose(archivoxml);
-		printf("GUARDO XML\n");
-	}
-	else
+	tElem* Aux;
+	FILE *arch;
+	int code = 0;
+	int nivel = 0; /* Se utiliza para saber cuantos 'tabs' imprimir en el archivo */
+	if (tElem_Inicializar(Aux) != 0)
 		return -1;
+	if ((arch = fopen(rutaXml, "w")) == NULL) { /* No se pudo abrir el archivo */
+		free(Aux);
+		return -2;
+	}
+	/* Escribo el tagPrincipal */
+	EscribirAtributo_Apertura(TDAXml->tagPrincipal,arch);
+	fprintf('\n');
+	EscribirTabs(++nivel, arch);
+	/* Comienzo a recorrer la lista de atributos */
+	if (L_Vacia(TDAXml->atributos)!=0) {
+		do {
+			code = L_Mover_Cte(&(TDAXml->atributos),L_Primero);
+			L_Elem_Cte(TDAXml->atributos,Aux);
+			if (EsDelimitador(Aux)==0) { /* Es un delimitador */
+				if (strcmp(Aux.Value,DELIM_OPEN)==0) { /* Es apertura */
+					EscribirAtributo_Apertura(Aux.Atributo, arch);
+					fprintf('\n');
+					EscribirTabs(++nivel,arch);
+				}
+				else { /* Es cierre */
+					EscribirAtributo_Cierre(Aux.Atributo, arch);
+					fprintf('\n');
+					EscribirTabs(--nivel,arch);
+				}
+			}
+			else { /* Es un elemento */
+				EscribirAtributo_Apertura(Aux.Atributo, arch);
+				fputs(Aux.Value);
+				EscribirAtributo_Cierre(Aux.Atributo, arch);
+				fprintf('\n');
+				EscribirTabs(nivel,arch);
+			}
+		} while ((code = L_Mover_Cte(&(TDAXml->atributos),L_Siguiente)) != 0)
+	}
+	/* Cierro el tagPrincipal */
+	EscribirAtributo_Cierre(TDAXml->tagPrincipal,arch);
+	/* Libero la memoria utilizada y cierro el archivo */
+	tElem_Destruir(Aux);
+	fclose(arch);
 	return 0;
-
 }
 
 void TDAXML_Destruir(TDAXML *TDAXml)
