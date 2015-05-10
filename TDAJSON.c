32432a3deb613578e6a32e0230e2a684449e6c76
Karina <kari-gisela@users.noreply.github.com>
2015-05-10 13:21:44 -0300

    Update JSONCargar
    Mejor, pero todavia no está diez puntos
    Ahora carga en la lista los "menuitem" intermedios

--- TDAJSON.c
+++ TDAJSON.c
@@ -51,13 +51,13 @@ int jsonCargar(TDAJSON *TDAJson, char *rutaJson)
 
 			while(letra != 34) /*mientras no cierre comilla, leo y guardo en Elem.id*/
 			{
-				Elem.id[i] = letra;
-				i++;
-				letra = fgetc(TDAJson->jsonFile);
+					Elem.id[i] = letra;
+					i++;
+					letra = fgetc(TDAJson->jsonFile);
 			}
 
 			Elem.id[i] = '\0';
-			printf("\n: %s", Elem.id);
+			printf("\n%s", Elem.id);
 
 			strcpy(palabra_ant, Elem.id);
 
@@ -72,39 +72,43 @@ int jsonCargar(TDAJSON *TDAJson, char *rutaJson)
 				strcpy(vector[tope], Elem.id);
 
 				Elem.estado = Abierto;
-				error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+				if (Elem.id != '\0')
+                    error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
 			}
 
 			if ((letra == ',') || (letra == '}'))
 			{
 				printf(" -VALOR-");
 				/*printf("\nTOPE: %d", tope);*/
-				printf("\nPALABRA SIN COMILLA: %s -CIERRA-", vector[tope]);
+				printf("\n%s -CIERRA-", vector[tope]);
 
 				Elem.estado = Valor;
-				error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+				if (Elem.id != '\0')
+                    error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
 
 				strcpy(Elem.id,vector[tope]);
 				Elem.estado = Cerrado;
-				error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+				if (Elem.id != '\0')
+                    error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
 
 				vector[tope][0] = '\0';
 				if (tope > 0)
 					tope--;
 			}
 
-			Elem.id[0] = '\0'; /*limpio la variable*/
+			Elem.id[0] = '\0';	/*limpio la variable*/
 			i = 0;
 
 		}
 
 		if (letra == '}')
 		{
-			printf("\nPALABRA SIN COMILLA: %s -CIERRA-", vector[tope]);
+			printf("\n%s -CIERRA-", vector[tope]);
 
 			strcpy(Elem.id,vector[tope]);
 			Elem.estado = Cerrado;
-			error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+			if (Elem.id != '\0')
+                error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
 
 			vector[tope][0] = '\0';
 			if (tope > 0)
@@ -137,7 +141,7 @@ int jsonCargar(TDAJSON *TDAJson, char *rutaJson)
 					Elem.id[i] = '\0';
 					/*vector[tope]=Elem.id;*/
 
-					printf("\n: %s", Elem.id);
+					printf("\n%s", Elem.id);
 
 					letra_ant = letra;
 					letra = fgetc(TDAJson->jsonFile); /*obtengo una letra mas y me fijo si es un ":"*/
@@ -145,20 +149,27 @@ int jsonCargar(TDAJSON *TDAJson, char *rutaJson)
 					if(letra == '}') /*si cierra la llave, tengo que cerrar y volver a abrir el id*/
 					{
 						printf(" -VALOR-");
-						printf("\nPALABRA SIN COMILLA: %s -CIERRA-", vector[tope]);
+						printf("\n%s -CIERRA-", vector[tope]);
 
 						Elem.estado = Valor;
-						error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+						if (Elem.id != '\0')
+                            error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
 
 						strcpy(Elem.id,vector[tope]);
 						Elem.estado = Cerrado;
-						error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+						if (Elem.id != '\0')
+                            error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
 
 						vector[tope][0] = '\0';
 						if (tope > 0)
 							tope--;
 
-						printf("\n: %s -CIERRA-", palabra_ant);
+						printf("\n%s -CIERRA-", palabra_ant);
+						strcpy(Elem.id,palabra_ant);
+						Elem.estado = Cerrado;
+						if (Elem.id != '\0')
+                            error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+
 						primero_multiple = FALSE;
 					}
 
@@ -170,21 +181,23 @@ int jsonCargar(TDAJSON *TDAJson, char *rutaJson)
 						strcpy(vector[tope], Elem.id);
 
 						Elem.estado = Abierto;
-
-						error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+                        if (Elem.id != '\0')
+                            error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
 					}
 
 					if ((letra == ',') && (letra_ant != '}'))
 					{
 						printf(" -VALOR-");
-						printf("\nPALABRA SIN COMILLA: %s -CIERRA-", vector[tope]);
+						printf("\n%s -CIERRA-", vector[tope]);
 
 						Elem.estado = Valor;
-						error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+						if (Elem.id != '\0')
+                            error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
 
 						strcpy(Elem.id,vector[tope]);
 						Elem.estado = Cerrado;
-						error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+						if (Elem.id != '\0')
+                            error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
 
 						vector[tope][0] ='\0';
 						if (tope > 0)
@@ -196,8 +209,14 @@ int jsonCargar(TDAJSON *TDAJson, char *rutaJson)
 
 				}
 
-				if ((letra == '{') && (primero_multiple == FALSE))
-					printf("\n: %s -ABRE-", palabra_ant);
+				if ((letra == '{') && (primero_multiple == FALSE)){
+					printf("\n%s -ABRE-", palabra_ant);
+					strcpy(Elem.id,palabra_ant);
+                    Elem.estado = Abierto;
+                    if (Elem.id != '\0')
+                        error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+
+				}
 
 				letra_ant = letra;
 				letra = fgetc(TDAJson->jsonFile);
