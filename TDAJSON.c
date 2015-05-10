c238a0ed173ae87b0cbd899b471856e4b4dc8814
Karina <kari-gisela@users.noreply.github.com>
2015-05-10 12:41:50 -0300

    JSONCargar nuevito

--- TDAJSON.c
+++ TDAJSON.c
@@ -7,108 +7,220 @@
 
 int jsonCargar(TDAJSON *TDAJson, char *rutaJson)
 {
-
-	char letra = 0;
-	int cont = 0;
-	int tag = 0;
-
+ 
 	TElem Elem;
-
-	while ((letra != '"') && (letra != EOF))
+ 
+	char letra;
+	char letra_ant;
+	char palabra_ant[CANTMAX]; /*la uso por si se repite con el corchete*/
+ 
+	int i = 0, tope = -1, error = 0;
+	int primero_multiple = TRUE; /*flag para saber si es la primera vez que se escribe un atributo multiple*/
+	char vector[4][CANTMAX];
+ 
+ 	/*inicializo*/
+	vector[0][0] = '\0';
+	vector[1][0] = '\0';
+	vector[2][0] = '\0';
+	vector[3][0] = '\0';
+ 
+	Elem.id[0] = 0; /* palabra */
+ 
+	if (!TDAJson->jsonFile)
 	{
-
-		letra = fgetc(TDAJson->jsonFile);
-
+ 
+		fprintf(stderr,"La ruta %s no es valida\n", rutaJson);
+ 
+		return 1;
+ 
 	}
-
-	while (letra != EOF)
+ 
+	printf("SE ABRIO EL ARCHIVO!\n");
+	letra = fgetc(TDAJson->jsonFile);
+ 
+	/*ANTES DE GUARDAR EN LA LISTA, HAY QUE CHEQUEAR SIEMPRE QUE EL ID NO SEA NULO*/
+ 
+	while(letra != EOF) /*mientras que no se termine el archivo*/
 	{
-
-		Elem.id[0] = 0;
-
-		if (letra == '"')
+ 
+		/*SI ESTA ENTRE COMILLAS*/
+		if (letra == 34) /*COMILLA*/ /*recorro hasta que cierre la comilla*/
 		{
-
+ 
 			letra = fgetc(TDAJson->jsonFile);
-
-			cont = 0;
-
-			while (letra != '"')
+ 
+			while(letra != 34) /*mientras no cierre comilla, leo y guardo en Elem.id*/
 			{
-
-				Elem.id[cont] = letra;
-
-				letra = fgetc(TDAJson->jsonFile);
-
-				cont++;
-
+					Elem.id[i] = letra;
+					i++;
+					letra = fgetc(TDAJson->jsonFile);
 			}
-
-			Elem.id[cont] = 0;
-
-			letra = fgetc(TDAJson->jsonFile);
-
-			if (tag == 0)
-			{
-
-				strcpy(TDAJson->tagPrincipal,Elem.id);
-
-				tag = 1;
-
-				printf("Tag Pcpal: %s\n",Elem.id);
-
-			}
-			else
-			{
-
-				printf("%s\n",Elem.id);
-
-			}
-
-		}
-		else
-		{
-
+ 
+			Elem.id[i] = '\0';
+			printf("\n: %s", Elem.id);
+ 
+			strcpy(palabra_ant, Elem.id);
+ 
+			/*obtengo una letra mas y me fijo si es un ":"*/
+			letra=fgetc(TDAJson->jsonFile);
+ 
 			if (letra == ':')
 			{
-
-				letra = fgetc(TDAJson->jsonFile);
-
-				if (letra == '"')
-				{
-
-					letra = fgetc(TDAJson->jsonFile);
-
-					cont = 0;
-
-					while (letra != '"')
-					{
-
-						Elem.id[cont] = letra;
-
-						letra = fgetc(TDAJson->jsonFile);
-
-						cont++;
-
-					}
-
-					Elem.id[cont] = 0;
-
-					printf("Valor = %s\n",Elem.id);
-
-					letra = fgetc(TDAJson->jsonFile);
-
-				}
+				printf(" -ABRE-");
+				tope++;
+ 
+				strcpy(vector[tope], Elem.id);
+ 
+				Elem.estado = Abierto;
+				error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
 			}
-			else
-				letra = fgetc(TDAJson->jsonFile);
-
+ 
+			if ((letra == ',') || (letra == '}'))
+			{
+				printf(" -VALOR-");
+				/*printf("\nTOPE: %d", tope);*/
+				printf("\nPALABRA SIN COMILLA: %s -CIERRA-", vector[tope]);
+ 
+				Elem.estado = Valor;
+				error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+ 
+				strcpy(Elem.id,vector[tope]);
+				Elem.estado = Cerrado;
+				error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+ 
+				vector[tope][0] = '\0';
+				if (tope > 0)
+					tope--;
+			}
+ 
+			Elem.id[0] = '\0';	/*limpio la variable*/
+			i = 0;
+ 
 		}
-
+ 
+		if (letra == '}')
+		{
+			printf("\nPALABRA SIN COMILLA: %s -CIERRA-", vector[tope]);
+ 
+			strcpy(Elem.id,vector[tope]);
+			Elem.estado = Cerrado;
+			error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+ 
+			vector[tope][0] = '\0';
+			if (tope > 0)
+				tope--;
+		}
+ 
+		if (letra == '[')
+		{
+			vector[tope][0] = '\0'; /*borro el multiple para no repetirlo en el cierre*/
+			if (tope > 0)
+				tope--;
+ 
+			while(letra != ']') /*mientras no cierre corchete*/
+			{
+ 
+				if (letra == 34) /*COMILLA*/ /*recorro hasta que cierre la comilla*/
+				{
+ 
+					letra_ant = letra;
+					letra = fgetc(TDAJson->jsonFile);
+ 
+					while (letra != 34) /*mientras no cierre comilla, leo y guardo en Elem.id*/
+					{
+						Elem.id[i] = letra;
+						i++;
+						letra_ant = letra;
+						letra = fgetc(TDAJson->jsonFile);
+					}
+ 
+					Elem.id[i] = '\0';
+					/*vector[tope]=Elem.id;*/
+ 
+					printf("\n: %s", Elem.id);
+ 
+					letra_ant = letra;
+					letra = fgetc(TDAJson->jsonFile); /*obtengo una letra mas y me fijo si es un ":"*/
+ 
+					if(letra == '}') /*si cierra la llave, tengo que cerrar y volver a abrir el id*/
+					{
+						printf(" -VALOR-");
+						printf("\nPALABRA SIN COMILLA: %s -CIERRA-", vector[tope]);
+ 
+						Elem.estado = Valor;
+						error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+ 
+						strcpy(Elem.id,vector[tope]);
+						Elem.estado = Cerrado;
+						error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+ 
+						vector[tope][0] = '\0';
+						if (tope > 0)
+							tope--;
+ 
+						printf("\n: %s -CIERRA-", palabra_ant);
+						primero_multiple = FALSE;
+					}
+ 
+ 
+					if (letra == ':')
+					{
+						printf(" -ABRE-");
+						tope++;
+						strcpy(vector[tope], Elem.id);
+ 
+						Elem.estado = Abierto;
+ 
+						error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+					}
+ 
+					if ((letra == ',') && (letra_ant != '}'))
+					{
+						printf(" -VALOR-");
+						printf("\nPALABRA SIN COMILLA: %s -CIERRA-", vector[tope]);
+ 
+						Elem.estado = Valor;
+						error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+ 
+						strcpy(Elem.id,vector[tope]);
+						Elem.estado = Cerrado;
+						error = L_Insertar_Cte(&TDAJson->atributos,L_Siguiente,&Elem);
+ 
+						vector[tope][0] ='\0';
+						if (tope > 0)
+							tope--;
+					}
+ 
+					Elem.id[0] = '\0'; /*limpio la variable*/
+					i = 0;
+ 
+				}
+ 
+				if ((letra == '{') && (primero_multiple == FALSE))
+					printf("\n: %s -ABRE-", palabra_ant);
+ 
+				letra_ant = letra;
+				letra = fgetc(TDAJson->jsonFile);
+ 
+			}
+ 
+		}
+ 
+		letra = fgetc(TDAJson->jsonFile);
+ 
 	}
-
+ 
+	Elem.id[i] = '\0';
+	i++;
+ 
+	fclose(TDAJson->jsonFile);
+	printf("\n\nCARGO JSON");
+ 
+	if (error != 1)
+		return error;
+ 
 	return 0;
-
+ 
 }
 
 int jsonGuardar(TDAJSON *TDAJson, char *rutaJson)
