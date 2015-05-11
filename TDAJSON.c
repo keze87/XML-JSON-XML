03295b9de9eeffc9b3ecf7fd0f3a37fa3572e9bc
gtulipani <gtulipani@hotmail.com>
2015-05-10 23:40:58 -0300

    Update TDAJSON.c

--- TDAJSON.c
+++ TDAJSON.c
@@ -4,6 +4,68 @@
 
 #define CANTMAX 255 /*tamaño maximo de linea*/
 #define TAM_ELEM 200
+#define DELIM_FIN ": {" /* String luego de un DELIM */
+#define ELEM_SEPARADOR ',' /* Caracter que separa múltiples elementos */
+#define ELEM_SEPARADOR_ATR_VAL ": " /* String que separa el Atributo y el Value de un elemento */
+
+/* Funcion que se encarga de escribir un atributo */
+void EscribirAtributo(char at[CANTMAX], FILE* arch)
+{
+    fputc('\"', arch);
+    fputs(at, arch);
+    fputc('\"', arch);
+}
+
+/* Funcion que escribe una cantidad de tabs en el archivo */
+void EscribirTabs_JSON(int cant, FILE* arch)
+{
+    cant *= 3;
+    while (cant-- > 0)
+        fputc('\t', arch);
+}
+
+/* Funcion que se encarga de escribir los tabs, cuando existen corchetes */
+void EscribirTabs_Corchetes_JSON(int nivel, int long_atributo, FILE* arch)
+{
+    EscribirTabs_JSON(nivel, arch); /* Escribo los tabs, sin el atributo */
+    while (long_atributo-- > 0)
+        fputc(' ', arch);
+}
+
+/* Funcion que se encarga de escribir los cierres finales */
+void EscribirCierre(int nivel, int long_atributo, FILE* arch)
+{
+    EscribirTabs_JSON(nivel, arch); /* Escribo los tabs, sin el atributo */
+    while (long_atributo-- > 0) {
+        fputc(' ', arch);
+    }
+    fputc('}', arch);
+    fputc('\n', arch);
+}
+
+/* Funcion que se encarga de incrementar LongAtributo, que se utilizará para:
+ * - Escribir tabs cuando existen corchetes
+ * - Escribir las llaves
+ */
+void AgregarLongitudAtributo(int *long_atributo,int sumando)
+{
+    /* Le sumo la longitud del atributo, y los 4 caracteres de las comillas, los dos puntos y el espacio */
+    *long_atributo += (4 + sumando);
+}
+
+void FinalizarCorchete(int *CA, int *ET, int *EC, int *CAN2, int* PC, FILE* arch)
+{
+    fputc(']', arch);
+    if (*CAN2 == TRUE) { /* Dos niveles de corchetes */
+        *CAN2 = FALSE;
+    }
+    else
+    *CA = FALSE;
+    *ET = TRUE;
+    *EC = FALSE;
+    *PC = TRUE;
+}
+
 
 int jsonCargar(TDAJSON *TDAJson, char *rutaJson)
 {
@@ -253,30 +315,276 @@ int jsonCargar(TDAJSON *TDAJson, char *rutaJson)
 
 int jsonGuardar(TDAJSON *TDAJson, char *rutaJson)
 {
-
-	FILE *archivojson;
-	archivojson = fopen(rutaJson, "w");
-
-	if (archivojson)
-	{
-
-		printf("SE ABRIO!\n");
-
-
-
-		fclose(archivojson);
-		printf("GUARDO JSON\n");
-
-	}
-	else
-		fprintf(stderr,"La ruta %s no es valida\n", rutaJson);
-
-	return 0;
-
-}
-
-void TDAJSON_Destruir(TDAJSON *TDAJson)
-{
+    TElem Aux;
+	FILE *arch;
+	char Atr_Aux[CANTMAX];
+	int code = 0;
+	int nivel = 0; /* Se utiliza para saber cuantos tabs imprimir en el archivo */
+	/* Defino booleanos a utilizar */
+	int LeerElemento = FALSE;
+	int EscribeTab = FALSE;
+	int EscribeLlave = FALSE;
+	int EsCerrado = FALSE;
+	int CoincidenAtributos = FALSE;
+	int CoincidenAtributos_Nivel2 = FALSE;
+	int PrimerCoincidencia = TRUE;
+	int EscribeCierre = FALSE;
+	/* Defino variables auxiliares */
+	int nivel_aux = 0; /* Se utiliza para almacenar el nivel de Atr_Aux */
+	int LongAtributo = 0; /* Acumula la longitud de los atributos cuando existen corchetes, por diseño */
+	int LongAtributo_Llave[3];
+	/* Acumula la longitud de los atributos cuando deben escribirse llaves, por diseño
+	 * Cada posición corresponde a un nivel, en total son 3 niveles
+	 * Si la cantidad de niveles fuera variable, en lugar de un vector sería conveniente utilizar el TDA_Pila
+	 */
+    char Atributo_Llave[CANTMAX];
+    char Atributo_Llave_Nivel2[CANTMAX];
+    /* Acumula los atributos cuando deben escribirse corchetes
+     * Si la cantidad de niveles fuera variable, en lugar de variables sería conveniente utilizar el TDA_Pila
+     */
+	int CantCierres = 0;
+	Atributo_Llave[0] = '\0';
+    Atributo_Llave_Nivel2[0] = '\0';
+    Atr_Aux[0] = '\0';
+	if ((arch = fopen(rutaJson, "w")) == NULL) /* No se pudo abrir el archivo */
+        return -1;
+    /* Escribo el tagPrincipal */
+    fputc('{', arch);
+    EscribirAtributo(TDAJson->tagPrincipal,arch);
+    fputs(DELIM_FIN, arch);
+    fputc('\n', arch);
+    LongAtributo_Llave[nivel] = (strlen(TDAJson->tagPrincipal));
+    EscribirTabs_JSON(++nivel, arch);
+    /* Comienzo a recorrer la lista de atributos */
+    if (L_Vacia(TDAJson->atributos) == 0) {
+        code = L_Mover_Cte(&(TDAJson->atributos),L_Primero);
+        L_Elem_Cte(TDAJson->atributos,&Aux);
+        do {
+            if (LeerElemento == TRUE) {
+                L_Elem_Cte(TDAJson->atributos,&Aux);
+                LeerElemento = FALSE;
+            }
+            switch (Aux.estado)
+            {
+                case(Abierto): /* Es apertura */
+                {
+                    if (EscribeTab == TRUE) { /* Luego de un corchete, se escribe un tab */
+                        if (CoincidenAtributos == TRUE)
+                            EscribirTabs_Corchetes_JSON(nivel, LongAtributo, arch);
+                        else
+                            EscribirTabs_JSON(nivel, arch);
+                        EscribeTab = FALSE;
+                    }
+                    if ((CoincidenAtributos == TRUE) && (PrimerCoincidencia == FALSE)) { /* Estoy dentro del corchete y no es el primero */
+                        fputc(ELEM_SEPARADOR, arch);
+                        fputc(' ', arch);
+                    }
+                    if (EscribeLlave == TRUE) {
+                        fputc('{', arch);
+                        EscribeLlave = FALSE;
+                    }
+                    EscribirAtributo(Aux.id, arch);
+                    fputs(ELEM_SEPARADOR_ATR_VAL, arch);
+                    /* Guardo el nombre del atributo y el nivel para buscar si se repite */
+                    strcpy(Atr_Aux,Aux.id);
+                    nivel_aux = nivel;
+                    L_Borrar_Cte(&(TDAJson->atributos));
+                    /* El corriente queda en el siguiente, cómo es apertura debe existir */
+                    L_Elem_Cte(TDAJson->atributos,&Aux);
+                    if (Aux.estado == Abierto) { /* Es otro de apertura, ya que no puede ser cerrado, debe tener valor */
+                        nivel ++;
+                        CantCierres++;
+                        /* Recorro la lista hasta encontrar el primer elemento del mismo nivel */
+                        EsCerrado = FALSE;
+                        while ((code != 0) && (nivel_aux < nivel)) {
+                            code = L_Mover_Cte(&(TDAJson->atributos),L_Siguiente);
+                            L_Elem_Cte(TDAJson->atributos,&Aux);
+                            switch(Aux.estado)
+                            {
+                                case(Abierto):
+                                {
+                                    if (EsCerrado == FALSE)
+                                        nivel++;
+                                    else
+                                        EsCerrado = FALSE;
+                                    break;
+                                }
+                                case(Cerrado):
+                                {
+                                    nivel--;
+                                    EsCerrado = TRUE;
+                                    break;
+                                }
+                                case(Valor):
+                                    nivel++;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        /* El corriente se encuentra en el mismo nivel, pero en el Cerrado con id = Atr_Aux */
+                        code = L_Mover_Cte(&(TDAJson->atributos),L_Siguiente);
+                        if (code != 0) { /* Existe otro elemento del mismo nivel */
+                            L_Elem_Cte(TDAJson->atributos,&Aux);
+                            if (strcmp(Atr_Aux,Aux.id) == 0) { /* Coinciden los atributos */
+                                AgregarLongitudAtributo(&LongAtributo,strlen(Aux.id));
+                                if (CoincidenAtributos == TRUE) {
+                                    CoincidenAtributos_Nivel2 = TRUE; /* Un corchete adentro de otro */
+                                    LongAtributo += 2;
+                                    strcpy(Atributo_Llave_Nivel2,Aux.id);
+                                }
+                                else {
+                                    strcpy(Atributo_Llave,Aux.id);
+                                }
+                                fputc('[', arch);
+                                fputc('{', arch);
+                                CoincidenAtributos = TRUE;
+                                PrimerCoincidencia = TRUE;
+                                CantCierres--;
+                            }
+                            else { /* No coinciden los atributos */
+                                fputc('{', arch);
+                                fputc('\n', arch);
+                                LongAtributo_Llave[nivel] = (strlen(Atr_Aux));
+                                EscribirTabs_JSON(++nivel, arch);
+                            }
+                        }
+                        else { /* Es el último elemento de su nivel */
+                                fputc('{', arch);
+                                fputc('\n', arch);
+                                LongAtributo_Llave[nivel] = (strlen(Atr_Aux));
+                                EscribirTabs_JSON(++nivel, arch);
+                        }
+                        code = L_Mover_Cte(&(TDAJson->atributos),L_Primero); /* Vuelvo al atributo posterior al repetido */
+                        L_Elem_Cte(TDAJson->atributos,&Aux);
+                    }
+                    break;
+                }
+                case(Cerrado): /* Es un cierre */
+                {
+                    if (CoincidenAtributos == FALSE) { /* No estoy dentro de los corchetes */
+                        if (EscribeCierre == FALSE) { /* No debo escribir el cierre cómo llave */
+                            L_Borrar_Cte(&(TDAJson->atributos));
+                            if (L_Vacia(TDAJson->atributos) == 0) {
+                                fputc(ELEM_SEPARADOR, arch);
+                                fputc('\n', arch);
+                                EscribirTabs_JSON(nivel,arch);
+                                L_Elem_Cte(TDAJson->atributos,&Aux);
+                            }
+                            else { /* Puede sucede que no termine con llave */
+                                fputc('\n', arch);
+                            }
+                        }
+                        else {
+                            nivel--;
+                            AgregarLongitudAtributo(&LongAtributo_Llave[nivel],0);
+                            EscribirCierre(nivel, LongAtributo_Llave[nivel], arch);
+                            L_Borrar_Cte(&(TDAJson->atributos));
+                            CantCierres--;
+                            EscribeCierre = FALSE;
+                            LeerElemento = TRUE; /* Puede sucede que sea el último elemento */
+                        }
+                    }
+                    else { /* Estoy dentro de los corchetes */
+                        if (EscribeTab == TRUE) { /* Si cerré corchete y tengo que escribir tab, eran 2 corchetes */
+                            EscribirTabs_Corchetes_JSON(nivel, LongAtributo, arch);
+                            EscribeTab = FALSE;
+                        }
+                        L_Borrar_Cte(&(TDAJson->atributos));
+                        L_Elem_Cte(TDAJson->atributos,&Aux);
+                        if (Aux.estado == Cerrado) { /* Termina la línea del corchete */
+                            fputc('}', arch);
+                            L_Borrar_Cte(&(TDAJson->atributos));
+                            L_Elem_Cte(TDAJson->atributos,&Aux);
+                            switch (Aux.estado)
+                            {
+                                case(Cerrado): /* Termina el corchete */
+                                {
+                                    FinalizarCorchete(&CoincidenAtributos,&EscribeTab,&EscribeCierre,&CoincidenAtributos_Nivel2,&PrimerCoincidencia,arch);
+                                    if (CoincidenAtributos == TRUE) { /* Eran dos niveles de corchetes */
+                                        LongAtributo -= strlen(Atributo_Llave_Nivel2) + 6; /* Puramente por diseño */
+                                        L_Borrar_Cte(&(TDAJson->atributos));
+                                        if (L_Vacia(TDAJson->atributos) == 0) { /* Aún existe un elemento más */
+                                            L_Elem_Cte(TDAJson->atributos,&Aux);
+                                            if (strcmp(Atributo_Llave,Aux.id) == 0) { /* El corriente corresponde al Abierto del primer nivel de corchete */
+                                                L_Borrar_Cte(&(TDAJson->atributos));
+                                                L_Elem_Cte(TDAJson->atributos,&Aux);
+                                                EscribeLlave = TRUE;
+                                                fputc(ELEM_SEPARADOR, arch);
+                                                LongAtributo++; /* Le sumo el correspondiente al '[' */
+                                            }
+                                            else {
+                                                fputc(']', arch);
+                                                fputc(ELEM_SEPARADOR, arch);
+                                                CoincidenAtributos = FALSE;
+                                            }
+                                        }
+                                        else { /* No existian más elementos */
+                                            fputc(']', arch);
+                                        }
+                                        fputc('\n', arch);
+                                    }
+                                    else {
+                                        LongAtributo -= strlen(Atributo_Llave);
+                                        fputc('\n', arch);
+                                    }
+                                    LeerElemento = FALSE;
+                                    break;
+                                }
+                                case (Abierto): /* Termina la línea y empieza otra nueva */
+                                {
+                                    if (strcmp(Aux.id,(CoincidenAtributos_Nivel2==FALSE)?Atributo_Llave:Atributo_Llave_Nivel2) == 0) {
+                                        /* Coinciden los atributos */
+                                        fputc(ELEM_SEPARADOR, arch);
+                                        fputc('\n', arch);
+                                        EscribirTabs_Corchetes_JSON(nivel, LongAtributo, arch);
+                                        fputc('{', arch);
+                                        PrimerCoincidencia = TRUE;
+                                        /* El corriente es otra vea Atr_Aux Abierto */
+                                        L_Borrar_Cte(&(TDAJson->atributos));
+                                        L_Elem_Cte(TDAJson->atributos,&Aux);
+                                    } /* No coincide el atributo */
+                                    else {
+                                        FinalizarCorchete(&CoincidenAtributos,&EscribeTab,&EscribeCierre,&CoincidenAtributos_Nivel2,&PrimerCoincidencia,arch);
+                                        fputc('\n', arch);
+                                        break;
+                                    }
+                                    break;
+                                }
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                    break;
+                }
+                case(Valor): /* Es un valor */
+                {
+                    EscribirAtributo(Aux.id, arch);
+                    L_Borrar_Cte(&(TDAJson->atributos));
+                    if (PrimerCoincidencia == TRUE)
+                        PrimerCoincidencia = FALSE;
+                    L_Elem_Cte(TDAJson->atributos,&Aux);
+                    break;
+                }
+                default:
+                    break;
+            }
+        } while (L_Vacia(TDAJson->atributos) == 0);
+    }
+    if (CantCierres > 0) { /* Al final hay una llave */
+        AgregarLongitudAtributo(&LongAtributo_Llave[1],0);
+        EscribirCierre(1, LongAtributo_Llave[1], arch);
+    }
+    /* Cierro el tagPrincipal */
+    AgregarLongitudAtributo(&LongAtributo_Llave[0],0);
+    LongAtributo_Llave[0]++; /* Cómo la primer línea incluye un '{', hay que agregar un caracter más */
+    EscribirCierre(0, LongAtributo_Llave[0], arch);
+    fputc('}', arch);
+    /* Cierro el archivo */
+	fclose(arch);
+	/* Destruyo la estructura TDAJSON */
 	free(TDAJson->tagPrincipal);
-	L_Vaciar(&(TDAJson->atributos));
+ 	L_Vaciar(&(TDAJson->atributos));
+	return 0;
 }
